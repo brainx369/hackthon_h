35f78b7354aa8d8c629ace3979d328eea160b0c0
brainx369 <advjava4me@gmail.com>
2026-02-28 16:58:44 +0530

    adding the search api and connecting with local postgress

---
+++ src/main/java/com/fulfilment/application/monolith/warehouses/dto/WarehouseFilterRequest.java
@@ -0,0 +1,8 @@
+package com.fulfilment.application.monolith.warehouses.dto;
+
+public class WarehouseFilterRequest {
+    public String name;
+    public String location;
+    public Integer minCapacity;
+    public Integer maxCapacity;
+}
